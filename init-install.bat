79cc491ba3fb635c00d60216d54a9cf9a1151a42
ivan232343 <ivanchiroque2@gmail.com>
2024-05-13 16:33:25 -0500

    chg install.bat

--- init-install.bat
+++ init-install.bat
@@ -1,2 +1,7 @@
+@echo off
+ECHO "Iniciando la instalacion de modulos"
 npm init
+ECHO "Instalando modulos"
 npm install
+set "DIR_ROOT=%~dp0"
+ECHO {"token": "MTIyMDgxNTg3ODE1Nzc2NjwNw.GwTVvk.vmeIagUK2_SgAGRoChTiXml8qraOaWOc6vwidw" , "guildId": ["1215716246172213398"],  "clientId": "1220815878157766807",  "channelId": "1117975646228971622",  "areaATcliente": "1215716247568912429",  "areaValidacion": "1221722912868794383",  "areaVAsesor": "1223082009551699969"  "host": "localhost",  "user": "ejecutor-gamer",  "password": "vi1uMa2AXu67",  "database": "bd_gamer_data" } > "%DIR_ROOT%config.json"
