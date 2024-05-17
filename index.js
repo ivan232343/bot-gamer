f350abd322d9c94cd8d37680473dc893f0467412
ivan232343 <ivanchiroque2@gmail.com>
2024-05-17 18:03:34 -0500

    delete comments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * Cod proyecto: proy-0035-2024-exp-win-revision-implementacion-discord-para-plan-gamer
  * fecha: 15/05/2024
  * motivo: 
- * Archivo raiz
+ * Archivo raiz  
  */
 const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
 const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits
@@ -11,8 +11,6 @@ const { Message, GuildMember, User, ThreadMember } = Partials
 const { token } = require('./config.json');
 const { selectLoader, eventsLoader, commandsLoader, buttonLoader, modalLoader } = require("./modules/Eventsloader");
 
-
-// AREA BOT LOADER 
 const client = new Client({
     intents: [Guilds, GuildMembers, GuildMessages],
     partials: [Message, User, GuildMember, ThreadMember]
@@ -22,17 +20,12 @@ client.commands = new Collection();
 client.buttons = new Collection();
 client.modals = new Collection();
 client.selects = new Collection();
-// eventGuildLoader(client);
-// eventServerLoader();
+eventsLoader(client);
+commandsLoader(client);
+buttonLoader(client);
+modalLoader(client);
+selectLoader(client);
 
-eventsLoader(client)
-commandsLoader(client)
-
-buttonLoader(client)
-modalLoader(client)
-selectLoader(client)
-// mainloader(client)
-
-client.login(token)
+client.login(token);
 
 
